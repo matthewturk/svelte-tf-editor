44838b8a71d68cb3bd681ae5a3008c526485420f
Matthew Turk <matthewturk@gmail.com>
2024-06-17 15:22:18 -0500

    Start adding types for girder folders

--- src/lib/girder.d.ts
+++ src/lib/girder.d.ts
@@ -14,3 +14,23 @@ export interface GirderCollection {
 export interface GirderCollectionDetails {
 	nFolders: number;
 }
+interface GirderJson {
+	[x: string]: string | number | boolean | Date | GirderJson | GirderJson[];
+}
+export interface GirderFolderDetails {
+	_accessLevel: number;
+	_id: string;
+	_modelType: string; // should be enum
+	baseParentId: string;
+	baseParentType: string; // should be enum
+	created: Date;
+	creatorId: string;
+	description: string;
+	meta?: GirderJson;
+	name: string;
+	parentCollection: string; // should be enum maybe?
+	parentId: string;
+	public: boolean;
+	size: number;
+	updated: Date;
+}
